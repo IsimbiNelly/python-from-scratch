64d0c16f0876886833ff6fb1a22254532bc3b8e8
IsimbiNelly <i.assoumpta@alustudent.com>
2026-01-20 03:46:45 +0200

    Day 1: Python basics and band name generator

---
+++ day_1/task.py
@@ -0,0 +1,21 @@
+# This is task one
+print("Hello World!")
+# This is task 2
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
+# This is task 3 of writing strings on different lines without having to repeatedly write print
+print("Hello my name is Nelly.\nI am a software engineering student at ALU.\nThis is my first python class. I hope I become an expert at this.")
+# This is task 4 of writing strings using a plus sign, what this does is combine two strings together
+print("Hello"+"Class")
+# I also learnt that IndentationError: unexpected indent means that I put extra space that was not needed. Spaces are very important while writing a python code.
+# This is task 5 where I will be using input code and add an exclamation mark at the end of the sentence.
+print("Hello "+input("What is your name?")+"!")
+# This is task 5 and I will be using len variable to show the number of characters in a string
+name= "Nelly"
+print(len(name))
+#This is the final project for day 1
+print("Welcome to the Band Name Generator")
+print("Your brand name could be "+ input("What's the name of the city you grew up in? ")+" "+input(" What's your pet's name? "))
